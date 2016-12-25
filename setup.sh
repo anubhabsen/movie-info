acc530ac478512590f7f00ee6c65de55e0b2714d
Anubhab Sen <anubhabsen@gmail.com>
2016-12-25 18:29:20 +0530

    Updates setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@ pip install lxml
 pip install requests
 pip install guessit
 
+echo
+echo
+echo
 echo "Dependencies installed, ready to run the program."
-echo "starting..."
+echo "starting in 5 seconds..."
+echo "Use command: python getInfo.py from this directory in the future"
+
+sleep 5
 
 python getInfo.py
