1c546a35ed3138e747d9cd4157b7d0a771346731
Anubhab Sen <anubhabsen@gmail.com>
2016-12-25 14:44:48 +0530

    Script to install necessary dependencies

---
+++ setup.sh
@@ -0,0 +1,10 @@
+pip install setuptools
+pip install pandas
+pip install lxml
+pip install requests
+pip install guessit
+
+echo "Dependencies installed, ready to run the program."
+echo "starting..."
+
+python getInfo.py
